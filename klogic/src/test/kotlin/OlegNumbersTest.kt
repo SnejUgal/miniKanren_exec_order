698261bf417991c64189849b6f758f93d8d9fb95
Kakadu <Kakadu@pm.me>
2023-06-07 13:22:38 +0300

    Copying Oleg numbers implementation to the source
    Signed-off-by: Kakadu <Kakadu@pm.me>

--- klogic/src/test/kotlin/OlegNumbersTest.kt
+++ klogic/src/test/kotlin/OlegNumbersTest.kt
@@ -3,10 +3,10 @@ import org.junit.jupiter.api.Test
 import org.klogic.core.Term
 import org.klogic.core.run
 import utils.UnificationsController
-import org.klogic.utils.terms.OlegLogicNumber
-import org.klogic.utils.terms.OlegLogicNumber.Companion.toOlegLogicNumber
-import org.klogic.utils.terms.expᴼ
-import org.klogic.utils.terms.logᴼ
+import utils.OlegLogicNumber
+import utils.OlegLogicNumber.Companion.toOlegLogicNumber
+import utils.expᴼ
+import utils.logᴼ
 
 class OlegNumbersTest {
     @AfterEach
@@ -16,19 +16,19 @@ class OlegNumbersTest {
 
     @Test
     fun testExpo1() {
-        val base = 3u.toOlegLogicNumber()
-        val power = 5u.toOlegLogicNumber()
+        val base = 2u.toOlegLogicNumber()
+        val power = 3u.toOlegLogicNumber()
 
         println("$base^$power")
-        run(1, { r: Term<OlegLogicNumber> -> expᴼ(base, power, r) })
+        run(1, { r: Term<utils.OlegLogicNumber> -> expᴼ(base, power, r) })
         UnificationsController.onFinish()
     }
 
     @Test
     fun testLogo1() {
-        val n = 14u.toOlegLogicNumber()
+        val n = 8u.toOlegLogicNumber()
         val b = 2u.toOlegLogicNumber()
-        val r = 5u.toOlegLogicNumber()
+        val r = 0u.toOlegLogicNumber()
 
         println("log $n base $b with reminder $r")
         run(1, { q: Term<OlegLogicNumber> -> logᴼ(n, b, q, r) })
