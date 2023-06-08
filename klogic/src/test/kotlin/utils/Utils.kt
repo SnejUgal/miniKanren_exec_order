af3ba1b485f7f927d14cee2df3122edf1eb3e0b4
Yury Kamenev <yurkam447@gmail.com>
2023-06-08 16:08:09 +0300

    Added permutations

--- klogic/src/test/kotlin/utils/Utils.kt
+++ klogic/src/test/kotlin/utils/Utils.kt
@@ -1,6 +1,7 @@
 package utils
 
 import org.klogic.core.*
+import org.klogic.core.Var.Companion.createTypedVar
 import org.klogic.utils.terms.LogicList
 import org.klogic.utils.terms.Nil.nilLogicList
 import org.klogic.utils.terms.plus
@@ -32,34 +33,99 @@ infix fun <T : Term<T>> Term<T>.debugUnify(other: Term<T>): Goal = { state: Stat
 
 typealias ListTerm<T> = Term<LogicList<T>>
 
+private var variableIndex: Int = 0
+
+fun <T : Term<T>> freshTypedVar(): Var<T> = (variableIndex++).createTypedVar()
+
+fun <T1 : Term<T1>> freshTypedVars(f: (Term<T1>) -> Goal): Goal = { st: State ->
+    val first = freshTypedVar<T1>()
+
+    delay { f(first) }(st)
+}
+
+fun <T1 : Term<T1>, T2 : Term<T2>> freshTypedVars(f: (Term<T1>, Term<T2>) -> Goal): Goal = { st: State ->
+    val first = freshTypedVar<T1>()
+    val second = freshTypedVar<T2>()
+
+    delay { f(first, second) }(st)
+}
+
+fun <T1 : Term<T1>, T2 : Term<T2>, T3 : Term<T3>> freshTypedVars(f: (Term<T1>, Term<T2>, Term<T3>) -> Goal): Goal = { st: State ->
+    val first = freshTypedVar<T1>()
+    val second = freshTypedVar<T2>()
+    val third = freshTypedVar<T3>()
+
+    delay { f(first, second, third) }(st)
+}
+
+fun <T1 : Term<T1>, T2 : Term<T2>, T3 : Term<T3>, T4 : Term<T4>> freshTypedVars(f: (Term<T1>, Term<T2>, Term<T3>, Term<T4>) -> Goal): Goal = { st: State ->
+    val first = freshTypedVar<T1>()
+    val second = freshTypedVar<T2>()
+    val third = freshTypedVar<T3>()
+    val fourth = freshTypedVar<T4>()
+
+    delay { f(first, second, third, fourth) }(st)
+}
+
+fun <T1 : Term<T1>, T2 : Term<T2>, T3 : Term<T3>, T4 : Term<T4>, T5 : Term<T5>> freshTypedVars(f: (Term<T1>, Term<T2>, Term<T3>, Term<T4>, Term<T5>) -> Goal): Goal = { st: State ->
+    val first = freshTypedVar<T1>()
+    val second = freshTypedVar<T2>()
+    val third = freshTypedVar<T3>()
+    val fourth = freshTypedVar<T4>()
+    val fifth = freshTypedVar<T5>()
+
+    delay { f(first, second, third, fourth, fifth) }(st)
+}
+
+fun <T1 : Term<T1>, T2 : Term<T2>, T3 : Term<T3>, T4 : Term<T4>, T5 : Term<T5>, T6 : Term<T6>> freshTypedVars(f: (Term<T1>, Term<T2>, Term<T3>, Term<T4>, Term<T5>, Term<T6>) -> Goal): Goal = { st: State ->
+    val first = freshTypedVar<T1>()
+    val second = freshTypedVar<T2>()
+    val third = freshTypedVar<T3>()
+    val fourth = freshTypedVar<T4>()
+    val fifth = freshTypedVar<T5>()
+    val sixth = freshTypedVar<T6>()
+
+    delay { f(first, second, third, fourth, fifth, sixth) }(st)
+}
+
+fun <T1 : Term<T1>, T2 : Term<T2>, T3 : Term<T3>, T4 : Term<T4>, T5 : Term<T5>, T6 : Term<T6>, T7 : Term<T7>> freshTypedVars(f: (Term<T1>, Term<T2>, Term<T3>, Term<T4>, Term<T5>, Term<T6>, Term<T7>) -> Goal): Goal = { st: State ->
+    val first = freshTypedVar<T1>()
+    val second = freshTypedVar<T2>()
+    val third = freshTypedVar<T3>()
+    val fourth = freshTypedVar<T4>()
+    val fifth = freshTypedVar<T5>()
+    val sixth = freshTypedVar<T6>()
+    val seventh = freshTypedVar<T7>()
+
+    delay { f(first, second, third, fourth, fifth, sixth, seventh) }(st)
+}
+
+fun <T1 : Term<T1>, T2 : Term<T2>, T3 : Term<T3>, T4 : Term<T4>, T5 : Term<T5>, T6 : Term<T6>, T7 : Term<T7>, T8 : Term<T8>> freshTypedVars(f: (Term<T1>, Term<T2>, Term<T3>, Term<T4>, Term<T5>, Term<T6>, Term<T7>, Term<T8>) -> Goal): Goal = { st: State ->
+    val first = freshTypedVar<T1>()
+    val second = freshTypedVar<T2>()
+    val third = freshTypedVar<T3>()
+    val fourth = freshTypedVar<T4>()
+    val fifth = freshTypedVar<T5>()
+    val sixth = freshTypedVar<T6>()
+    val seventh = freshTypedVar<T7>()
+    val eighth = freshTypedVar<T8>()
+
+    delay { f(first, second, third, fourth, fifth, sixth, seventh, eighth) }(st)
+}
+
 fun <T : Term<T>> appendo(a: ListTerm<T>, b: ListTerm<T>, ab: ListTerm<T>): Goal = { state ->
     System.out.printf("appendo: %s %s %s\n", a, b, ab)
 
     conde(
         and(
-            (a debugUnify nilLogicList()).let {
-                println("Appendo: a === () - $a === ()")
-                it
-            },
-            (b debugUnify ab).let {
-                println("Appendo: b === ab - $b === $ab")
-                it
-            }
+            (a debugUnify nilLogicList()),
+            (b debugUnify ab)
         ),
         freshTypedVars<T, LogicList<T>, LogicList<T>> { head, tail, rest ->
             and(
-                (a debugUnify head + tail).let {
-                    println("Appendo: a === h + t - $a === ${head + tail}")
-                    it
-                },
-                (ab debugUnify head + rest).let {
-                    println("Appendo: ab === h + r - $ab === ${head + rest}")
-                    it
-                },
-                appendo(tail, b, rest).let {
-                    println("Appendo: appendo(tail, b, rest) - appendo($tail, $b, $rest)")
-                    it
-                }
+                (a debugUnify head + tail),
+                (ab debugUnify head + rest),
+                appendo(tail, b, rest)
             )
         }
     )(state)
@@ -70,33 +136,18 @@ fun <T : Term<T>> reverso(a: ListTerm<T>, b: ListTerm<T>): Goal = { state ->
 
     conde(
         and(
-            (a debugUnify nilLogicList()).let {
-                println("Reverso: a === () - $a === ()")
-                it
-              },
-            (a debugUnify b).let {
-                println("Reverso: a === b - $a === $b")
-                it
-            }
+            (a debugUnify nilLogicList()),
+            (a debugUnify b)
         ),
         freshTypedVars<T, LogicList<T>, LogicList<T>> { h, t, rest ->
             and(
-                (a debugUnify h + t).let {
-                    println("Reverso: a === h + t - $a === $h + $t")
-                    it
-                },
-                reverso(t, rest).let {
-                    println("Reverso: reverso(t, rest) - reverso($t, $rest)")
-                    it
-                },
+                (a debugUnify h + t),
+                reverso(t, rest),
                 appendo(
                     rest,
                     h.toLogicList(),
                     b
-                ).let {
-                    println("Reverso: appendo(rest, (h), b) - appendo($rest, ($h), $b)")
-                    it
-                }
+                )
             )
         }
     )(state)
